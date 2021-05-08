d721f1d9ee96acd1dc137c3c794d4518e44d9491
valentinaandrade <valentinaandrade@uchile.cl>
2021-05-08 13:28:40 -0400

    create requerimientos
    @nicolasggodoym  y @ecuadroszenteno  deben correr esta sintaxis que contiene algunos de los paquetes y funciones con las que deben trabajar.

---
+++ R/00-requerimientos.R
@@ -0,0 +1,17 @@
+# Code requierements ------------------------------------------------------
+
+if (!require("remotes")) install.packages("remotes")
+
+if (!require("OCDE")) remotes::install_github("expersso/OECD")
+
+if (!require("Rilostat")) remotes::install_github("ilostat/Rilostat", force = TRUE)
+
+# CRAN
+paquetes <- c("tidyverse", "WDI","haven", "rvest", "kableExtra", "sjlabelled",
+              "pdftools", "ggsci", "readxl", "readsdmx")
+
+for(p in paquetes) {
+  
+  if (!require(p)) install.packages(p)
+  
+}
