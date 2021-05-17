f6db22b47f372e444a2d8be5e71895216bca7edd
valentinaandrade <valentinaandrade@uchile.cl>
2021-05-17 19:49:14 -0400

    iloeplx nico

---
+++ R/08-iloeplex.R
@@ -0,0 +1,45 @@
+# Code: ILOEPLX --------------------------------------------------------
+# 1. Install packages -----------------------------------------------------
+pacman::p_load(Rilostat, tidyverse,rvest)
+
+# 2. Scrapping variables ------------------------------------------------------------
+fct <- read_html('https://eplex.ilo.org/fixed-term-contracts-ftcs/') %>% 
+  html_node('.table') %>% 
+  html_table() %>% 
+  select(year = "Year(s)", country_name = 3,
+         ftc_reg = 7, fct_valid = 10, fct_max_nocum = 13, fct_max = 16)
+
+## repetir proceso
+
+# 3. Recode and rename---------------------------------------------------------------
+##Instrucciones: Similar a ocde02
+
+
+# %>%
+#   mutate_at(vars(fct_valid,fct_max_nocum), funs(as_factor(.))))
+
+## recode country_name https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+
+
+# 4. Merge ----------------------------------------------------------------
+
+## Merge all EPLEx------------------------------------
+data_ilo <- lapply(ls(),get)
+data_ilo <- Reduce(function(x,y) merge (x, y, by = c("country_name", "year")))
+
+## Merge and scrap with iso ----------------------------
+iso <- read_html('https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2') %>% 
+  html_node('.wikitable.sortable') %>% 
+  html_table() %>%
+  group_by(Code) %>% 
+  filter(Year == max(Year)) %>% 
+  select(iso2c=Code, country_name = "Country name (using title case)")
+
+data_iloeplx <- merge(data_ilo, iso,
+                  by = "country_name")
+
+# 5. Label ----------------------------------------------------------------
+
+# 6. Select ------------------------------------------------------
+
+# 8. Save -----------------------------------------------------------------
