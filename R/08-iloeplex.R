defa4f44f282bf65cb920448752ce3fcd41fe5f0
Nicolás Godoy M <nicolas.godoy.m@ug.uchile.cl>
2021-05-17 22:57:41 -0400

    Update 08-iloeplex.R

--- R/08-iloeplex.R
+++ R/08-iloeplex.R
@@ -1,18 +1,41 @@
 # Code: ILOEPLX --------------------------------------------------------
 # 1. Install packages -----------------------------------------------------
-pacman::p_load(Rilostat, tidyverse,rvest)
+pacman::p_load(Rilostat, tidyverse,rvest, car)
 
 # 2. Scrapping variables ------------------------------------------------------------
-fct <- read_html('https://eplex.ilo.org/fixed-term-contracts-ftcs/') %>% 
+ftc <- read_html('https://eplex.ilo.org/fixed-term-contracts-ftcs/') %>% 
   html_node('.table') %>% 
   html_table() %>% 
   select(year = "Year(s)", country_name = 3,
-         ftc_reg = 7, fct_valid = 10, fct_max_nocum = 13, fct_max = 16)
+         ftc_reg = 7, ftc_valid = 10, ftc_max_nocum = 13, ftc_max = 16)
 
 ## repetir proceso
 
 # 3. Recode and rename---------------------------------------------------------------
 ##Instrucciones: Similar a ocde02
+
+ftc$ftc_reg <-  recode(ftc$ftc_reg, "c('Y')=1; c('N')=2") 
+ftc$ftc_reg <-  factor(ftc$ftc_reg, 
+                labels = c("Yes", "No"))
+
+ftc$ftc_valid <- recode(ftc$ftc_valid, "'no limitation'=0; 
+                        'no limitation on first FTC'=1; 
+                        'objective and material reasons' = 2")
+
+ftc$ftc_valid <- factor(ftc$ftc_valid,
+                        labels=c('No limitation', 'No limitation on first FTC',
+                                 'Objective and material reasons'))
+
+ftc$ftc_max_nocum <- recode(ftc$ftc_max_nocum, "'no limitation'=0; 
+                        'no limitation on first FTC'=1; 
+                        'objective and material reasons' = 2")
+
+ftc$ftc_max_nocum <- factor(ftc$ftc_max_nocum,
+                        labels=c('No limitation', 'No limitation on first FTC',
+                                 'Objective and material reasons'))
+
+
+ftc$ftc_max <- recode(ftc$ftc_max, "'no limitation'=0")
 
 
 # %>%
