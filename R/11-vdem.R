c82273a24942ab3f1640bca14c987b2346e43f49
Nicolás Godoy M <nicolas.godoy.m@ug.uchile.cl>
2021-06-20 23:51:05 -0400

    vdem update

--- R/11-vdem.R
+++ R/11-vdem.R
@@ -19,10 +19,40 @@ vdem <- vdem %>%
   select(iso3c = country_text_id, year,variable_original)
 
 # 5. Recode -----------------------------------------------------------------
-vdem <- vdem %>% 
-  mutate_at(vars(v2elcomvot:v2x_regime,e_boix_regime), funs(as_factor(.))) %>% 
-  mutate(e_boix_regime = car::recode(e_boix_regime, c("0='No';1='Yes'"), as.factor = T,
-                                     levels = c("No", "Yes")))
+vdem$e_boix_regime <- car::recode(vdem$e_boix_regime, recodes = c("0='No';1='Yes'"), as.factor = T,
+                                     levels = c("No", "Yes"))
+
+vdem$v2elcomvot <- car::recode(vdem$v2elcomvot, recodes = c("1 = 'Yes. But there are no sanctions or sanctions are not enforced';
+                                                       2 = 'Yes. Sanctions exist and are enforced, but they impose minimal costs upon the offending voter';
+                                                       3 = 'Yes. Sanctions exist, they are enforced, and they impose considerable costs upon the offending voter'"),
+                                     as.factor = T, levels = c('Yes. But there are no sanctions or sanctions are not enforced',
+                                                               'Yes. Sanctions exist and are enforced, but they impose minimal costs upon the offending voter',
+                                                               'Yes. Sanctions exist, they are enforced, and they impose considerable costs upon the offending voter')) 
+
+vdem$v2elfemrst <-  car::recode(vdem$v2elfemrst, recodes = c("0 = 'No female suffrage';
+                                               1 = 'Restricted female suffrage';
+                                               2 = 'Universal female suffrage'"), as.factor = T,
+                                 levels = c("No female suffrage", "Restricted female suffrage", "Universal female suffrage")) 
+
+vdem$v3peminwage <- car::recode(vdem$v3peminwage, recodes = c("0 = 'No';
+                                                 1 = 'Yes, the state imposes a minimum wage';
+                                                 2 = 'Yes, there are corporate bargaining arrangements that effectively ensure a minimum wage';
+                                                 3 = 'Yes, there is a tripartite committee that sets the wage'"),
+                                  as.factor = T, levels = c('No', 'Yes, the state imposes a minimum wage',
+                                                            'Yes, there are corporate bargaining arrangements that effectively ensure a minimum wage',
+                                                            'Yes, there is a tripartite committee that sets the wage')) 
+
+vdem$v2x_regime <- car::recode(vdem$v2x_regime, recodes = c("0 = 'Closed autocracy';
+                                               1 = 'Electoral autocracy';
+                                               2 = 'Electoral democracy';
+                                               3 = 'Liberal democracy'"),
+                                 as.factor = T, levels = c('Closed autocracy',
+                                                           'Electoral autocracy',
+                                                           'Electoral democracy',
+                                                           'Liberal democracy')) 
+  
+  
+              
 # ISO3C ----------------------------------------------------------------
 # x <- vdem %>%
 #   mutate(iso3c = countrycode(country_id, "vdem", "iso3c")) %>% 
